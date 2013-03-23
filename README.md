80cfbe0a5bd78af5b5f220cb472f273f5005cfcd
Tracy Wadleigh <tracy.wadleigh@gmail.com>
2013-03-23 12:56:36 -0400

    Retargeted at fixed-length immutable vectors (bounding the scope).

--- README.md
+++ README.md
@@ -1,52 +1,7 @@
-Geometry.jl
-===========
+Vecs.jl
+=======
 
-Basic geometric primitives and operations.
+Fixed-length immutable vectors.
 
-
-Current state
--------------
-
-A type for 2D and 3D points plus some basic arithmetic.
-
-
-Ambitions
----------
-
-This library is currently little more than a stub, but may grow to include:
-
-* the common bits for the various visualization efforts;
-* representations, conversions, and operations for a variety of mathematical
-and geophysical coordinate systems;
-* computational geometry algorithms.
-
-Big ideas welcome. 
-Also welcome are sensible ideas about what the right scope for this
-package is, versus what should be left for other dependent packages.
-One thing I would like to consider to bound the scope of this library 
-is to keep it pure Julia.
-
-
-Possible sources of inspiration
--------------------------------
-
-* [__CGAL (Computational Geometry Algorithms Library)__](http://www.cgal.org).
-Pluses: 
-Fairly comprehensive. 
-Algorithmically rigorous.
-Minuses: 
-C++ interface would probably require a C binding layer.
-GPL license is more restrictive than the MIT License of Julia.
-* [__Qhull__](http://www.qhull.org). 
-Plus: the core has undergone years of exercise in the real world.
-Minus: both the C and C++ interfaces are considered incomplete/provisional 
-according to the website.
-* __Matlab, Octave, R.__ Matlab and Octave, it seems, do little more than
-wrap Qhull. R may have some packages that do more. Hopefully some of the
-R expats in the Julia community will be able to weigh in on this.
-* [__GDAL (Geospatial Data Abstraction Library)__](http://www.osgeo.org/gdal_ogr). 
-For when we consider the geospatial problem. 
-(Maybe best left to a downstream package.)
-
-
-
+Currently supports vectors of length 2 through 4, but may eventually be
+macrofied to allow easy instantiation for a larger number of dimensions.
---
+++ src/Vecs.jl
@@ -0,0 +1,138 @@
+module Vecs
+
+
+# Basic types
+
+immutable Vec2{T}
+    x :: T
+    y :: T
+end
+immutable Vec3{T}
+    x :: T
+    y :: T
+    z :: T
+end
+immutable Vec4{T}
+    x :: T
+    y :: T
+    z :: T
+    w :: T
+end
+export Vec2, Vec3, Vec4
+
+
+# Type aliases
+
+typealias Vec2f Vec2{Float32}
+typealias Vec3f Vec3{Float32}
+typealias Vec4f Vec4{Float32}
+typealias Vec2d Vec2{Float64}
+typealias Vec3d Vec3{Float64}
+typealias Vec4d Vec4{Float64}
+typealias Mat22{T} Vec2{Vec2{T}}
+typealias Mat23{T} Vec2{Vec3{T}}
+typealias Mat24{T} Vec2{Vec4{T}}
+typealias Mat32{T} Vec3{Vec2{T}}
+typealias Mat33{T} Vec3{Vec3{T}}
+typealias Mat34{T} Vec3{Vec4{T}}
+typealias Mat42{T} Vec4{Vec2{T}}
+typealias Mat43{T} Vec4{Vec3{T}}
+typealias Mat44{T} Vec4{Vec4{T}}
+typealias Mat2{T} Mat22{T}
+typealias Mat3{T} Mat33{T}
+typealias Mat4{T} Mat44{T}
+typealias Mat22f Vec2{Vec2f}
+typealias Mat23f Vec2{Vec3f}
+typealias Mat24f Vec2{Vec4f}
+typealias Mat32f Vec3{Vec2f}
+typealias Mat33f Vec3{Vec3f}
+typealias Mat34f Vec3{Vec4f}
+typealias Mat42f Vec4{Vec2f}
+typealias Mat43f Vec4{Vec3f}
+typealias Mat44f Vec4{Vec4f}
+typealias Mat2f Mat22f
+typealias Mat3f Mat33f
+typealias Mat4f Mat44f
+typealias Mat22d Vec2{Vec2d}
+typealias Mat23d Vec2{Vec3d}
+typealias Mat24d Vec2{Vec4d}
+typealias Mat32d Vec3{Vec2d}
+typealias Mat33d Vec3{Vec3d}
+typealias Mat34d Vec3{Vec4d}
+typealias Mat42d Vec4{Vec2d}
+typealias Mat43d Vec4{Vec3d}
+typealias Mat44d Vec4{Vec4d}
+typealias Mat2d Mat22d
+typealias Mat3d Mat33d
+typealias Mat4d Mat44d
+
+export Vec2f, Vec3f, Vec4f, Vec2d, Vec3d, Vec4d, 
+Mat22, Mat23, Mat24, Mat32, Mat33, Mat34, 
+Mat42, Mat43, Mat44, Mat2, Mat3, Mat4, 
+Mat22f, Mat23f, Mat24f, Mat32f, Mat33f, Mat34f, 
+Mat42f, Mat43f, Mat44f, Mat2f, Mat3f, Mat4f, 
+Mat22d, Mat23d, Mat24d, Mat32d, Mat33d, Mat34d, 
+Mat42d, Mat43d, Mat44d, Mat2d, Mat3d, Mat4d 
+
+
+# Indexing
+
+import Base.getindex
+
+function getindex{T}(v::Vec2{T}, i::Integer)
+    if i == 1
+        return v.x
+    elseif i == 2 
+        return v.y
+    else
+        throw("Invalid index for Vec2: $i")
+    end
+end
+
+function getindex{T}(v::Vec3{T}, i::Integer)
+    if i == 1
+        return v.x
+    elseif i == 2 
+        return v.y
+    elseif i == 3 
+        return v.z
+    else
+        throw("Invalid index for Vec3: $i")
+    end
+end
+
+function getindex{T}(v::Vec4{T}, i::Integer)
+    if i == 1
+        return v.x
+    elseif i == 2 
+        return v.y
+    elseif i == 3 
+        return v.z
+    elseif i == 4 
+        return v.w
+    else
+        throw("Invalid index for Vec4: $i")
+    end
+end
+
+
+# Basic operations
+
+importall Base
+
++{T}(v1::Vec2{T},v2::Vec2{T}) = Vec2{T}(v1.x+v2.x,v1.y+v2.y)
+-{T}(v1::Vec2{T},v2::Vec2{T}) = Vec2{T}(v1.x-v2.x,v1.y-v2.y)
+.*{T}(v1::Vec2{T},v2::Vec2{T}) = Vec2{T}(v1.x*v2.x,v1.y*v2.y)
+./{T}(v1::Vec2{T},v2::Vec2{T}) = Vec2{T}(v1.x/v2.x,v1.y/v2.y)
+*{T}(s::T,v::Vec2{T}) = Vec2{T}(s*v.x,s*v.y)
+/{T}(v::Vec2{T},s::T) = Vec2{T}(v.x/s,v.y/s)
+
++{T}(v1::Vec3{T},v2::Vec3{T}) = Vec3{T}(v1.x+v2.x,v1.y+v2.y,v1.z+v2.z)
+-{T}(v1::Vec3{T},v2::Vec3{T}) = Vec3{T}(v1.x-v2.x,v1.y-v2.y,v1.z-v2.z)
+*{T}(v1::Vec3{T},v2::Vec3{T}) = Vec3{T}(v1.x*v2.x,v1.y*v2.y,v1.z*v2.z)
+/{T}(v1::Vec3{T},v2::Vec3{T}) = Vec3{T}(v1.x/v2.x,v1.y/v2.y,v1.z/v2.z)
+*{T}(s::T,v::Vec3{T}) = Vec3{T}(s*v.x,s*v.y,s*v.z)
+/{T}(v::Vec3{T},s::T) = Vec3{T}(v.x/s,v.y/s,v.z/s)
+
+
+end
