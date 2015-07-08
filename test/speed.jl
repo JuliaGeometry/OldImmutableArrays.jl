ff443c4ff6d58e8d9ea65121a5964ed3a61820c9
Steve Kelly <kd2cca@gmail.com>
2015-07-08 16:28:49 -0500

    cosmetics

--- test/speed.jl
+++ test/speed.jl
@@ -1,20 +1,20 @@
 using ImmutableArrays
 
 function ratio(f::Function, a1, a2, b1, b2, iters::Integer)
-	if f(a1, a2) != f(b1, b2)
-		return string("result mismatch:\n", f(a1, a2), "\n--- vs. ---\n", f(b1, b2))
-	end
-	tic()
-	for i = 1:iters
-		f(a1, a2)
-	end
-	atime = toq()
-	tic()
-	for i = 1:iters
-		f(b1, b2)
-	end
-	btime = toq()
-	return btime / atime
+    if f(a1, a2) != f(b1, b2)
+        return string("result mismatch:\n", f(a1, a2), "\n--- vs. ---\n", f(b1, b2))
+    end
+    tic()
+    for i = 1:iters
+        f(a1, a2)
+    end
+    atime = toq()
+    tic()
+    for i = 1:iters
+        f(b1, b2)
+    end
+    btime = toq()
+    return btime / atime
 end
 ratio(f::Function, a1, a2, b1, b2) = ratio(f, a1, a2, b1, b2, 1000000)
 mat4ratio(f::Function, m1, m2) = ratio(f, m1, m2, Matrix4x4(m1), Matrix4x4(m2))
